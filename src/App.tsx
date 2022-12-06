198412ae0a293da5aa7ceac3fcdb7a60cf8bafec
luis <lferreira646@gmail.com>
2022-12-06 15:12:19 -0300

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import Header from "./components/Header";
 
 function App() {
-
   return (
-    <h1>Hello world!</h1>
+    <>
+      <Header />
+      <main>
+        <section>
+          
+        </section>
+      </main>
+    </>
   );
 }
 
-export default App
+export default App;
